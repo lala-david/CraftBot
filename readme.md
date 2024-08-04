33f7630b73466da17026a05dd289ee29173a0817
seongjun <wnswns1946@naver.com>
2024-08-04 21:27:01 +0900

    # 👷🏼 commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+## Labeling bot
+
+###  🛰️ Version 1 
+![alt text](assets/image.png)
+
+#### Total Time: 5M
+
+### 🍀 Version 2
+![alt text](assets/label.png)
+
+#### Total Time: 18s
+
---
+++ src/embeddings.py
@@ -0,0 +1,16 @@
+import json
+from sklearn.metrics.pairwise import cosine_similarity
+from sentence_transformers import SentenceTransformer
+
+embeddings = SentenceTransformer('all-MiniLM-L6-v2')
+
+def get_embedding(text):
+    return embeddings.encode(text)
+
+def calculate_category_embeddings(all_category_labels):
+    return [get_embedding(label) for label in all_category_labels]
+
+def find_most_similar_category(label_embedding, category_embeddings, all_category_labels):
+    similarities = cosine_similarity([label_embedding], category_embeddings)
+    category_index = similarities.argmax()
+    return all_category_labels[category_index]
